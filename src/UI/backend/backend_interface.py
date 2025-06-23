58cd867b32646f0eb2ae1f8b1e1e2183eae1b1dc
Swapnil-Trivedi <swapniltrivedi1456@gmail.com>
2025-06-23 18:16:11 +0000

    integrating tool router

--- src/UI/backend/backend_interface.py
+++ src/UI/backend/backend_interface.py
@@ -1,8 +1,11 @@
 import requests
-import random
+import logging
+
 class BackendInterface:
     def __init__(self, backend_url: str):
         self.backend_url = backend_url
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO)
 
     def ping(self):
         response = requests.get(f"{self.backend_url}/ping")
@@ -14,12 +17,12 @@ class BackendInterface:
         return response.json()
 
 
-    def calculate(self):
-        response = requests.post(f"{self.backend_url}/calculator")
+    def calculate(self,query: str):
+        response = requests.post(f"{self.backend_url}/calculator", params={"query": query})
         return response.json()
 
-    def json_format(self):
-        response = requests.post(f"{self.backend_url}/json_formatter")
+    def json_format(self,query: str = None):
+        response = requests.post(f"{self.backend_url}/json_formatter", params={"query": query})
         return response.json()
 
     def translator(self, text: str):
@@ -27,15 +30,18 @@ class BackendInterface:
         return response.json()
 
     def infer(self, input_text: str):
-        # payload = {"input_text": input_text}
-        # response = requests.post(f"{self.backend_url}/infer", json=payload)
-        # return response.json()
          # MOCKED: Randomly return a classify label
-        random_class = random.choice([ "translate", "calculator", "json_formatter"])
-        return {
-            "classify": random_class,
-            "processed_text": input_text
-        }
+        # random_class = random.choice([ "translate", "calculator", "json_formatter"])
+        # return {
+        #     "classify": random_class,
+        #     "processed_text": input_text
+        # }
+        try:
+            payload = {"input_text": input_text}
+            response = requests.post(f"{self.backend_url}/infer_t5", json=payload)
+            return response.json()
+        except Exception as e:
+            return {"error": str(e)}    
 
     def get_agent_response(self, user_input: str, file_bytes: bytes = None, file_name: str = None):
         """
@@ -49,24 +55,24 @@ class BackendInterface:
         Returns:
             dict: response from the selected backend tool
         """
-        classification = self.infer(user_input)
-
-        classify = classification.get("classify", "").lower()
-        processed_text = classification.get("processed_text", user_input)
-
-        if classify == "ocr":
-            if file_path is None:
-                return {"error": "OCR requires an image file."}
-            return self.get_ocr(file_path)
-
-        elif classify == "translate":
-            return self.translator(processed_text)
-
-        elif classify == "calculator":
-            return self.calculate()
-
-        elif classify == "json_formatter":
-            return self.json_format()
-
+        self.logger.info(f"User Input: {user_input}")
+        if user_input.lower() == "ping":
+            response=self.ping()
+            return response.get("response","")
+        # call the infer endpoint to classify the user input
+        classify_response = self.infer(user_input)
+        self.logger.info(f"Classify Response: {classify_response}")
+        response=classify_response.get("response", "")
+        self.logger.info(f"Response from classify: {response}")
+        #call tool endpoints based on classification tool in string
+        if "calculate" in response.lower():
+            response = self.calculate(response)
+        elif "json" in response.lower():
+            response = self.json_format(response)
+        elif "translator" in response.lower():
+            response = self.translator(response)
+        elif "ocr" in response.lower() and file_bytes is not None and file_name is not None:
+            response = self.get_ocr(file_name, file_bytes)
         else:
-            return {"error": f"Unknown classification from /infer: '{classify}'"}
+            response = {"response": "No valid tool found for the input."}
+        return response.get("response", "")
